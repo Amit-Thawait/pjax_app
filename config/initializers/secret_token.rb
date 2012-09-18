fa5df20a4fdc83c5207d0bb859132a864b616f1b
Amit Thawait <amit17thawait@gmail.com>
2012-09-18 16:10:50 +0530

    PJAX Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PjaxApp::Application.config.secret_token = '9a58a0ea647c2a4ae8c86af0cb0ce931d0110d7380f6d8758e061cc61a88dd753b2407e880b0c7865649fbead2f96363a10faa3d7e199cb5f8c9ef90f3f4b87c'
